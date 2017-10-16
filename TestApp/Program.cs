b90fa53127ebd2217ba766fb565e4aea4db62083
Tom Needham <06needhamt@gmail.com>
2017-10-16 11:19:05 +0100

    Can now sent test email

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,15 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
 using System.Threading.Tasks;
 
 namespace TestApp
 {
-    class Program
+     public class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            Execute().Wait();
+            Console.ReadKey();
+        }
+
+        static async Task Execute()
+        {
+            var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("test@example.com", "Example User");
+            var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress("test@example.com", "Example User");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+            Console.WriteLine(response.StatusCode);
         }
     }
 }
